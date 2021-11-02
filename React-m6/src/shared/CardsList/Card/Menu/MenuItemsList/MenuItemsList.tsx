7e756a67d43d2b683d5e952e1e4c149fd4edaf99
Корлыханов Дмитрий Викторович <korlyhanovdv@morflot.local>
2021-11-02 22:04:10 +0300

    Module 5 complete fixed 1

--- React-m6/src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
+++ React-m6/src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { BlockIcon, WarningIcon, CommentsIcon, SaveIcon, SharedIcon } from '../../../../Icons';
+// import { BlockIcon, WarningIcon, CommentsIcon, SaveIcon, SharedIcon } from '../../../../Icons';
+import { Icon, EIcons } from '../../../../Icon';
 import styles from './menuitemslist.css';
 import { Text, EColor } from '../../../../Text';
 
@@ -11,27 +12,27 @@ export function MenuItemsList({ postId }: IMenuItemsListProps) {
     return (
         <ul className={styles.menuItemsList}>
             <li className={styles.menuItem} onClick={() => console.log(postId)}>
-                <CommentsIcon />
+                <Icon Name={EIcons.comments} size={12} />
                 <Text size={12} color={EColor.grey99}>Комментарии</Text>
             </li>
             <div className={styles.divider} />
             <li className={styles.menuItem} onClick={() => console.log(postId)}>
-                <SharedIcon />
+                <Icon Name={EIcons.shared} size={12} />
                 <Text size={12} color={EColor.grey99}>Поделиться</Text>
             </li>
             <div className={styles.divider} />
             <li className={styles.menuItem}>
-                <WarningIcon />
+                <Icon Name={EIcons.block} size={12} />
                 <Text size={12} color={EColor.grey99}>Скрыть</Text>
             </li>
             <div className={styles.divider} />
             <li className={styles.menuItem} onClick={() => console.log(postId)}>
-                <SaveIcon />
+                <Icon Name={EIcons.save} size={12} />
                 <Text size={12} color={EColor.grey99}>Сохранить</Text>
             </li>
             <div className={styles.divider} />
             <li className={styles.menuItem}>
-                <WarningIcon />
+                <Icon Name={EIcons.warning} size={12} />
                 <Text size={12} color={EColor.grey99}>Пожаловаться</Text>
             </li>
         </ul>
