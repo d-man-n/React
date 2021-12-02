d8826a777812381f591d0a53832bdd905e51a88b
d-man-n <73164141+d-man-n@users.noreply.github.com>
2021-12-02 12:01:06 +0300

    Delete ReactJS-ssr directory

--- ReactJS-ssr/src/client/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Header } from '../shared/Header'
-
-window.addEventListener('load', () => {
-    ReactDOM.hydrate(<Header />, document.getElementById('react_root'));
-});
-
