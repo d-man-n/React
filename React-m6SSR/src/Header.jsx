1621c6b71a3858abf031506c99e510fb1dbe765c
d-man-n <73164141+d-man-n@users.noreply.github.com>
2021-12-02 12:00:54 +0300

    Delete React-m6SSR directory

--- React-m6SSR/src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './header.css';
-
-export const Header = () => {
-    return (
-    <div className={styles.header}>Hellow from React component!!!123</div>
-    );
-}
-
--- React-m6SSR/src/client/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'react-hot-loader';
-import { Header } from '../Header';
-
-ReactDOM.hydrate(
-    <Header />, document.getElementById('react_root')
-);
